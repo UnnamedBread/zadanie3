7d261466f8df596fd064f3cf6a01be215c289af9
UnnamedBread <91851637+UnnamedBread@users.noreply.github.com>
2021-10-22 23:58:40 +0300

    Add files via upload

---
+++ Homework3.3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int rec(int n, int a = 2) {
+    if (a == 2) {
+        while (n % a == 0) {
+            cout << a << " ";
+            n = n / a;
+        }
+        if (n == 1)
+            return 0;
+        rec(n, a + 1);
+
+    }
+    else {
+        while (n % a == 0) {
+            cout << a << " ";
+            n = n / a;
+        }
+        if (n == 1)
+            return 0;
+        rec(n, a + 2);
+    }
+    return(0);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+    cin >> n;
+    rec(n);
+
+    return(0);
+}
